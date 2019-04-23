dc4482a9a82f76031df5e511e49241c8d24bbbdf
Noktoborus <noktoborus@gmail.com>
2019-04-24 01:31:27 +0300

    first result

--- app/src/main/java/ru/styxheim/wsang/MainActivity.java
+++ app/src/main/java/ru/styxheim/wsang/MainActivity.java
@@ -1,28 +1,27 @@
 package ru.styxheim.wsang;
 
-import android.view.View;
 import android.app.*;
 import android.os.*;
 import android.widget.*;
 import android.view.*;
 
-public class MainActivity extends Activity 
+public class MainActivity extends Activity
 {
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.start);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_start);
+  }
 
-	public void startOnClick(View v)
-	{
-		//TableLayout tl_crew = findViewById(R.id.start_table_crew);
-		TableLayout tl_time = findViewById(R.id.start_table_time);
-		LayoutInflater inflater = this.getLayoutInflater();
-		//View tr_crew = inflater.inflate(R.layout.start_row_crew, null);
+  public void startOnClick(View v)
+  {
+    TableLayout tl_crew = findViewById(R.id.start_table_crew);
+    TableLayout tl_time = findViewById(R.id.start_table_time);
+    LayoutInflater inflater = this.getLayoutInflater();
+    View tr_crew = inflater.inflate(R.layout.start_row_crew, null);
     View tr_time = inflater.inflate(R.layout.start_row_time, null);
-		//tl_crew.addView(tr_crew);
-		tl_time.addView(tr_time);
-	}
+    tl_crew.addView(tr_crew);
+    tl_time.addView(tr_time);
+  }
 }
