4ea166ec6c26a0111bbc4451f56edfe4ecc3b47b
Noktoborus <noktoborus@gmail.com>
2019-04-23 11:17:19 +0300

    first experience

--- app/src/main/java/ru/styxheim/wsang/MainActivity.java
+++ app/src/main/java/ru/styxheim/wsang/MainActivity.java
@@ -1,7 +1,10 @@
 package ru.styxheim.wsang;
 
+import android.view.View;
 import android.app.*;
 import android.os.*;
+import android.widget.*;
+import android.view.*;
 
 public class MainActivity extends Activity 
 {
@@ -9,6 +12,17 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.start);
     }
+
+	public void startOnClick(View v)
+	{
+		//TableLayout tl_crew = findViewById(R.id.start_table_crew);
+		TableLayout tl_time = findViewById(R.id.start_table_time);
+		LayoutInflater inflater = this.getLayoutInflater();
+		//View tr_crew = inflater.inflate(R.layout.start_row_crew, null);
+    View tr_time = inflater.inflate(R.layout.start_row_time, null);
+		//tl_crew.addView(tr_crew);
+		tl_time.addView(tr_time);
+	}
 }
