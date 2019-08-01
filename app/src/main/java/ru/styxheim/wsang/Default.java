810075c702c5d7c2cbc5a4df988b765b7990947f
Noktoborus <noktoborus@gmail.com>
2019-08-01 14:33:12 +0300

    Add forgotten `Defaul` class with default settings

---
+++ app/src/main/java/ru/styxheim/wsang/Default.java
@@ -0,0 +1,10 @@
+package ru.styxheim.wsang;
+
+import android.view.KeyEvent;
+
+public class Default {
+  final static public String mode = Launcher.Mode.START.name();
+  final static public String server_addr = "127.0.0.1";
+  final static public long chrono_offset = 0;
+  final static public int chrono_key = KeyEvent.KEYCODE_VOLUME_UP;
+};
