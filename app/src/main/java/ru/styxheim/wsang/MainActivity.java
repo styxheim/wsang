726c562a9cc4c3cda6f4e5df25d549208a8230a0
Noktoborus <noktoborus@gmail.com>
2019-11-06 15:09:52 +0300

    Allow to run over lockscreen

--- app/src/main/java/ru/styxheim/wsang/MainActivity.java
+++ app/src/main/java/ru/styxheim/wsang/MainActivity.java
@@ -71,6 +71,7 @@ public class MainActivity extends Activity
     settingsRace = getSharedPreferences("race", Context.MODE_PRIVATE);
     settingsChrono = getSharedPreferences("chrono", Context.MODE_PRIVATE);
     setContentView(R.layout.main);
+    setShowWhenLocked(true);
   }
 
   @Override
--- app/src/main/java/ru/styxheim/wsang/PenaltyActivity.java
+++ app/src/main/java/ru/styxheim/wsang/PenaltyActivity.java
@@ -57,6 +57,7 @@ public class PenaltyActivity extends Activity
     }
 
     _setupPenalties();
+    setShowWhenLocked(true);
   }
 
   protected void _setupPenalties()
